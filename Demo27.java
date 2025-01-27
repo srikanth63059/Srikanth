da5f711e2e0f98a62654f46309d29328eb504b94
srikanth63059 <2311cs010157@mallareddyuniversity.ac.in>
2025-01-27 12:54:05 +0530

    prime number

---
+++ Demo27.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Demo27{
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int n = scanner.nextInt();
+
+        if (isPrime(n)) {
+            System.out.println(n + " is a prime number.");
+        } else {
+            System.out.println(n + " is not a prime number.");
+        }
+    }
+
+    public static boolean isPrime(int num) {
+        if (num <= 1) return false;
+        for (int i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i == 0) return false;
+        }
+        return true;
+    }
+}
