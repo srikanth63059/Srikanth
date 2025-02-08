16d9241e80056dc37f72185de862a89948c14232
srikanth63059 <2311cs010157@mallareddyuniversity.ac.in>
2025-02-08 12:39:18 +0530

    Swapping numbers

--- Demo015.java
+++ Demo015.java
@@ -1,17 +1,23 @@
-class SwapDemo {
-	void swapnumbers() {
-		int a = 100, b = 200;
-		System.out.println("Before Swap a = "+a +" b = "+b);
-		int temp = a;
-		a = b;
-		b = temp;
-		System.out.println("After Swap a = "+a +" b = "+b);		
-	}
-}
+package Core_Java;
 
-public class Demo15 {
-    public static void main(String[] args){
-        SwapDemo obj = new SwapDemo();
-        obj.swapnumbers();
+import java.util.*;
+
+class Demo015{
+    void swapNumber(int a,int b){
+        System.out.println("Before Swapping:\na = "+ a+"\nb = "+b);
+        int temp = a;
+        a=b;
+        b=temp;
+        System.out.println("After Swapping:\na = "+ a+"\nb = "+b);
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Demo015 obj = new Demo015();
+
+        System.out.println("Enter 2 numbers: ");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        obj.swapNumber(a, b);
     }
 }
