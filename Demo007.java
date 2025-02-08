37002f7708bb0122ce1a9faa8437dd105509a23a
srikanth63059 <2311cs010157@mallareddyuniversity.ac.in>
2025-02-08 12:52:33 +0530

    User input

--- Demo007.java
+++ Demo007.java
@@ -1,11 +1,14 @@
+package Core_Java;
 
-public class Demo6{
-    public static void main(String[] args){
-        String username =  "charan";
-        String password =  "charan@123";
-        while((username == "charan") && (password == "charan@123")){
-            System.out.println("Hello charan welcome to Mallareddy University");
-            break;
-        }
-}
+import java.util.*;
+
+public class Demo007 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter name, age, phone number: ");
+        String name= sc.next();
+        String age=sc.next();
+        String num = sc.next();
+        System.out.println("Hello "+name+", age = "+age+", phone number: "+num);
+    }
 }
