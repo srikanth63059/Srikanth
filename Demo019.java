dc84cffac8eebf26660ef38a1f4819fe711f3e9d
srikanth63059 <2311cs010157@mallareddyuniversity.ac.in>
2025-02-08 12:43:32 +0530

    Fibonacci series

--- Demo019.java
+++ Demo019.java
@@ -1,11 +1,23 @@
-import java.util.Scanner;
+package Core_Java;
 
-public class Demo19 {
+import java.util.*;
+class Demo019{
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter a number: ");
-        double number = scanner.nextDouble();
-        long rounded = Math.round(number);
-        System.out.println("Rounded figure: " + rounded);
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number of digits: ");
+        int n=sc.nextInt();
+        Demo020 d = new Demo020();
+        d.getFibonacciSeries(n);
+    }
+    void getFibonacciSeries(int n){
+        System.out.println("Fibonacci series: ");
+        int a=0;
+        int b=1;
+        for(int i=0;i<n;i++){
+            System.out.print(a+" ");
+            int c=a+b;
+            a=b;
+            b=c;
+        }
     }
 }
