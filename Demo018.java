ad9b421a29c72981cceca67f97a3f5a760d76b62
srikanth63059 <2311cs010157@mallareddyuniversity.ac.in>
2025-02-08 12:42:43 +0530

    minutes to year and days

--- Demo018.java
+++ Demo018.java
@@ -1,17 +1,14 @@
-import java.util.Scanner;
+package Core_Java;
 
-public class Demo18 {
+import java.util.*;
+
+class Demo018{
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.print("Enter the number of minutes: ");
-        long minutes = scanner.nextLong();
-
-        long minutesInYear = 60 * 24 * 365;
-        long years = minutes / minutesInYear;
-        long remainingMinutes = minutes % minutesInYear;
-        long days = remainingMinutes / (60 * 24);
-
-        System.out.println(minutes + " minutes is approximately " + years + " years and " + days + " days.");
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number of minutes: ");
+        long minutes = sc.nextLong();
+        long years = minutes / (60 * 24 * 365);
+        long remainingDays = (minutes % (60 * 24 * 365)) / (60 * 24);
+        System.out.println("Years: "+years+" Days: "+remainingDays);
     }
 }
