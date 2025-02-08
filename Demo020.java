70bfa0fac2ce023d7d0648cbc308bd693325c1c0
srikanth63059 <2311cs010157@mallareddyuniversity.ac.in>
2025-02-08 12:44:20 +0530

    FizzBizz

--- Demo020.java
+++ Demo020.java
@@ -1,35 +1,27 @@
-import java.util.Scanner;
+package Core_Java;
 
-public class Demo20 {
+import java.util.*;
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        // Prompt the user for the number of terms
-        System.out.print("Enter the number of terms for the Fibonacci series: ");
-        int terms = scanner.nextInt();
-
-        // Validate input
-        if (terms <= 0) {
-            System.out.println("Please enter a positive integer.");
-        } else {
-            System.out.println("Fibonacci Series up to " + terms + " terms:");
-            generateFibonacci(terms);
+class FizzBizz{
+    void FizzBizz1(int i){
+        
+        if(i%3!=0&&i%5!=0){
+            System.out.println(String.valueOf(i));
+        }else if(i%3==0&&i%5==0){
+            System.out.println("FizzBuzz");
+        }else if(i%3==0&&i%5!=0){
+            System.out.println("Fizz");
+        }else if(i%3!=0&&i%5==0){
+            System.out.println("Buzz");
         }
-
-        scanner.close();
     }
-
-    // Method to generate and print the Fibonacci series
-    public static void generateFibonacci(int terms) {
-        int first = 0, second = 1;
-
-        for (int i = 1; i <= terms; i++) {
-            System.out.print(first + " ");
-            int next = first + second;
-            first = second;
-            second = next;
-        }
-        System.out.println();
+}
+class Demo020{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int n = sc.nextInt();
+        FizzBizz obj = new FizzBizz();
+        obj.FizzBizz1(n);
     }
 }
