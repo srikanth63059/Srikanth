30a2e7ffa4185164b13ee01a0cfac875cfb4ee72
srikanth63059 <2311cs010157@mallareddyuniversity.ac.in>
2025-02-09 22:16:11 +0530

    Print 1st 10 Prime numbers

---
+++ Demo061.java
@@ -0,0 +1,20 @@
+public class Demo061 {
+    public static void main(String[] args) {
+        System.out.println("Prime numbers between 1 and 10:");
+
+        for (int num = 2; num <= 10; num++) {
+            boolean isPrime = true;
+
+            for (int i = 2; i <= num / 2; i++) {
+                if (num % i == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+
+            if (isPrime) {
+                System.out.println(num);
+            }
+        }
+    }
+}
