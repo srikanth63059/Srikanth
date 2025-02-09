d40a61445d5ebb18eeb71e96ea9142b5ea705208
srikanth63059 <2311cs010157@mallareddyuniversity.ac.in>
2025-02-09 22:20:14 +0530

    printing of index values of an array

---
+++ Demo066.java
@@ -0,0 +1,11 @@
+public class Demo066 {
+    public static void main(String[] args) {
+
+        int[] numbers = { 10, 20, 30, 40, 50 };
+
+        System.out.println("Index : Value");
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.println(i + " : " + numbers[i]);
+        }
+    }
+}
